1826712f004c895b3fe4ea0d10bbef12eba61064
JSGGY <alekseinavalforever@gmail.com>
2025-10-13 17:46:19 -0500

    Add .gitmodules and implement calculator functionality

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,11 @@
+import { suma } from './Suma/suma.js';
+import { resta } from './Resta/resta.js';
+
+// Ejemplos de uso
+console.log('=== CALCULADORA ===');
+console.log('=== Suma ===');
+console.log('Suma: 8 + 2 =', suma(8, 2));
+console.log('Suma: 10 + 4 =', suma(10, 4));
+console.log('=== Resta ===');
+console.log('Resta: 12 - 10 =', resta(12, 10));
+console.log('Resta: 20 - 10 =', resta(20, 10));
